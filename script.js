7ebf055018b0606cb24dc45199dfadedaddf5188
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-07 23:11:40 +1100

    Add generic dispatcher function called operate()

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+
+let numberOne = 0;
+let numberTwo = 0;
+let operator = '';
+
+function operate(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+
+        default:
+            return "ERROR"
+    }
+}
