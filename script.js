d54f745343c0f816912002b52521588e7fb0725d
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-08 01:15:54 +1100

    Implement rough calculator functionality
    The calculator works reasonably well and correctly displays input
    digits and performs the correct operations on them. It also correctly
    displays the result.
    
    However, there is currently an issue where if you try to enter
    a new number after a successful calculation and then an operator
    and a second number, the calculator will always use the previous result
    instead of the new first number. This should be a relatively easy fix
    with the use of a flag variable or some reworking of the logic.
    
    Also need to refactor some repetitive code into functions, particularly
    the code involved when the operator and equals button are pressed.

--- script.js
+++ script.js
@@ -16,17 +16,19 @@ function divide(num1, num2) {
 }
 
 
-let numberOne = 0;
-let numberTwo = 0;
+let numberOne = "";
+let numberTwo = "";
 let operator = '';
+let operatorBuffer = '';
 let currDisplay = '';
 
 const display = document.querySelector("#display")
 
 function operate(op, num1, num2) {
+
     switch (op) {
         case '+':
-            return add(num1, num2);
+            return add(num1, num2)
         case '-':
             return subtract(num1, num2)
         case '*':
@@ -40,15 +42,98 @@ function operate(op, num1, num2) {
 }
 
 function setupButtonEventHandlers() {
-    digitButtons = document.querySelectorAll(".digit");
+
+    // Digit buttons
+    const digitButtons = document.querySelectorAll(".digit");
 
     digitButtons.forEach(button => {
         button.addEventListener("click", () => {
-            display.innerHTML += button.innerHTML;
-            currDisplay += button.innerHTML;
+            updateDisplay(button.innerHTML)
             //console.log(`currDisplay = ${currDisplay}`)
         });
     });
+
+    // = Button
+    const equalsButton = document.querySelector("#btn-equals")
+
+    equalsButton.addEventListener("click", () => {
+        numberTwo = Number(currDisplay)
+        clearDisplay()
+        let result = operate(operator, numberOne, numberTwo)
+        updateDisplay(result)
+        currDisplay = ""
+        clearData();
+        numberOne = result
+        numberTwo = null
+    })
+
+    // AC button
+    const clearButton = document.querySelector("#btn-clear")
+
+    clearButton.addEventListener("click", () => {
+        clearDisplay();
+        clearData();
+    })
+
+    // Operator buttons
+
+    const opButtons = [
+        document.querySelector("#btn-add"),
+        document.querySelector("#btn-subtract"),
+        document.querySelector("#btn-multiply"),
+        document.querySelector("#btn-divide")
+    ];
+
+    // Add event listeners using forEach
+    opButtons.forEach(button => {
+        button.addEventListener("click", () => handleOperatorInput(button.id));
+    });
+}
+
+function handleOperatorInput(op) {
+
+    console.log(op)
+
+    op == "btn-add" ? op = '+' :
+        op == "btn-subtract" ? op = '-' :
+            op == "btn-multiply" ? op = '*' :
+                op == "btn-divide" ? op = '/' :
+                    op == ''
+
+    // if the user has already provided an operator, perform a calculation
+    // while buffering this operator for the next calculation
+    if (operator) {
+        operatorBuffer = op;
+        clearDisplay()
+        let result = operate(operator, numberOne, numberTwo)
+        numberOne = result
+        numberTwo = null
+        operator = operatorBuffer
+        operatorBuffer = ""
+        updateDisplay(result)
+
+    } else {
+        // only register an operator if digits have been input
+        operator = op
+        numberOne != "" ? numberTwo = Number(currDisplay) : numberOne = Number(currDisplay)
+        currDisplay = ""
+    }
+}
+
+function updateDisplay(string) {
+    currDisplay += string
+    display.innerHTML = currDisplay
+}
+
+function clearDisplay() {
+    currDisplay = ""
+    display.innerHTML = ""
+}
+
+function clearData() {
+    operator = ""
+    numberOne = ""
+    numberTwo = ""
 }
 
 setupButtonEventHandlers();
